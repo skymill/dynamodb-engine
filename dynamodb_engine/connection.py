3b91201cea7ca0d7b4332669e5a79c644a8c7067
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-03-04 20:09:43 +0100

    Added support for connecting to DynamoDB Local

--- dynamodb_engine/connection.py
+++ dynamodb_engine/connection.py
@@ -2,9 +2,34 @@
 Connection handling
 """
 from boto.dynamodb2 import connect_to_region
+from boto.dynamodb2.layer1 import DynamoDBConnection
+
+from dynamodb_engine.exceptions import (
+    MissingDynamoDBLocalHostError,
+    MissingDynamoDBLocalPortError)
 
 
-def connect(region):
+def connect(meta):
+    """
+    Connect to AWS or DynamoDB Local
+
+    :type meta: dynamodb_engine.models.ModelMeta
+    :param meta: Model meta data object
+    :returns: boto.dynamodb2.layer1.DynamoDBConnection
+    """
+    if meta.dynamodb_local:
+        if not meta.dynamodb_local['host']:
+            raise MissingDynamoDBLocalHostError
+        if not meta.dynamodb_local['port']:
+            raise MissingDynamoDBLocalPortError
+        return connect_local(
+            meta.dynamodb_local['host'],
+            meta.dynamodb_local['port'])
+
+    return connect_aws(meta.dynamodb_local.region)
+
+
+def connect_aws(region):
     """
     Create a connection to DynamoDB
 
@@ -14,5 +39,26 @@ def connect(region):
     """
     try:
         return connect_to_region(region)
+    except Exception:
+        raise
+
+
+def connect_local(host, port):
+    """
+    Connect to DynamoDB Local
+
+    :type host: str
+    :param host: Hostname
+    :type port: int
+    :param port: Port number
+    :returns: boto.dynamodb2.layer1.DynamoDBConnection
+    """
+    try:
+        return DynamoDBConnection(
+            host=host,
+            port=port,
+            aws_access_key_id='foo',
+            aws_secret_access_key='bar',
+            is_secure=False)
     except Exception:
         raise
