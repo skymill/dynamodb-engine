9915c4d78faa593462d8853c46bab843759a147d
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-03-05 21:25:42 +0100

    Wrote test for hash+range tables

--- dynamodb_engine/test_models.py
+++ dynamodb_engine/test_models.py
@@ -75,6 +75,36 @@ class TestModel(unittest.TestCase):
 
         test.delete_table()
 
+    def test_create_table_hash_range(self):
+        """
+        Test that a basic table can be created. Both hash key and range key
+        """
+        class Test(Model):
+            class Meta:
+                table_name = 'test'
+                dynamodb_local = {
+                    'host': DYNAMODB_HOST,
+                    'port': DYNAMODB_PORT
+                }
+
+            username = StringAttribute('username', hash_key=True)
+            post = StringAttribute('post', range_key=True)
+
+        test = Test()
+        test.username = 'myuser'
+        test.post = 'example'
+        test.create_table()
+        key_schemas = test.describe_table()['Table']['KeySchema']
+
+        self.assertEqual(len(key_schemas), 2)
+        for key_schema in key_schemas:
+            if key_schema['KeyType'] == 'HASH':
+                self.assertEqual(key_schema['AttributeName'], 'username')
+            if key_schema['KeyType'] == 'RANGE':
+                self.assertEqual(key_schema['AttributeName'], 'post')
+
+        test.delete_table()
+
     def test_missing_hash_key(self):
         """
         Check if a missing hash key raises an exception
