7180a4b6f3e6aa5649078f892cc21a360799312c
Sebastian Dahlgren <sebastian.dahlgren@gmail.com>
2015-03-05 21:21:36 +0100

    Added create table with hash key test

--- dynamodb_engine/test_models.py
+++ dynamodb_engine/test_models.py
@@ -11,10 +11,62 @@ DYNAMODB_HOST = 'localhost'
 DYNAMODB_PORT = 8000
 
 
+class User(Model):
+    """ Basic User model used for testing purposes """
+    class Meta:
+        table_name = 'users'
+        dynamodb_local = {
+            'host': DYNAMODB_HOST,
+            'port': DYNAMODB_PORT
+        }
+
+    email = StringAttribute('email', hash_key=True)
+    first_name = StringAttribute('firstName')
+    last_name = StringAttribute('lastName')
+
+
 class TestModel(unittest.TestCase):
     """
     Unit tests for the model
     """
+    def setUp(self):
+        """ Set up method """
+        self.user = User()
+        self.user.email = 'sebastian.dahlgren@skymill.se'
+        self.user.first_name = 'Sebastian'
+        self.user.last_name = 'Dahlgren'
+        self.user.create_table()
+        self.user.save()
+
+    def tearDown(self):
+        """ Tear down method """
+        self.user.delete_table()
+
+    def test_create_table_hash(self):
+        """
+        Test that a basic table can be created. Only hash key
+        """
+        class Test(Model):
+            class Meta:
+                table_name = 'test'
+                dynamodb_local = {
+                    'host': DYNAMODB_HOST,
+                    'port': DYNAMODB_PORT
+                }
+
+            username = StringAttribute('username', hash_key=True)
+
+        test = Test()
+        test.username = 'myuser'
+        test.create_table()
+        key_schema = test.describe_table()['Table']['KeySchema']
+
+        self.assertEqual(len(key_schema), 1)
+        self.assertEqual(key_schema[0]['KeyType'], 'HASH')
+        self.assertEqual(key_schema[0]['AttributeName'], 'username')
+
+        test.delete_table()
+
     def test_missing_hash_key(self):
         """
         Check if a missing hash key raises an exception
@@ -35,4 +87,4 @@ class TestModel(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main()
